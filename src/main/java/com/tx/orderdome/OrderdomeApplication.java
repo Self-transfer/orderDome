59db791359a6878f6df585833f4f836db93411e5
tellmeanswer <tianxinzuilihai@gmail.com>
2020-01-02 17:23:33 +0800

    first commit

---
+++ src/main/java/com/tx/orderdome/OrderdomeApplication.java
@@ -0,0 +1,13 @@
+package com.tx.orderdome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderdomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderdomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tx/orderdome/OrderdomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tx.orderdome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderdomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
